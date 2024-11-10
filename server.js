684b1b4894f0f407935b186abf9513085e62d652
Nandu <80321802+NanduPastham123@users.noreply.github.com>
2024-11-10 13:30:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const collectionsRouter = require('./routes/collectionsRoute');
+const collectionsRouter = require('./collectionsRoute');
 
 const app = express();
 app.use(express.json());
