868ebee2d50e890b1f2f5390a9b7f09b7a4dcf97
Nandu <80321802+NanduPastham123@users.noreply.github.com>
2024-11-10 13:14:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const collectionsRouter = require('./routes/collectionsRoute');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/collections', collectionsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
